f1af6c30b0544b5062b478a79fff5e374ceaefd1
BraulioMndz87 <200021026+BraulioMndz87@users.noreply.github.com>
2026-02-04 21:10:47 -0600

    Testing

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,53 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup()
+
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "clangd",
+      },
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end,
+    })
+
+    vim.lsp.config("lua_ls", {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+
+    vim.lsp.config("pyright", {})
+
+    vim.lsp.config("clangd", {})
+
+    vim.lsp.config("ts_ls", {})
+
+    vim.lsp.enable({
+      "lua_ls",
+      "pyright",
+      "clangd",
+      "ts_ls",
+    })
+  end,
+}
+
