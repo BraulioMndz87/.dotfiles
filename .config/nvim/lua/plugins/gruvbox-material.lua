c356ef9e896a571f7fed88352bc48242fa8f8344
BraulioMndz87 <200021026+BraulioMndz87@users.noreply.github.com>
2026-07-02 19:10:03 -0600

    Nvim

--- .config/nvim/lua/plugins/gruvbox-material.lua
+++ .config/nvim/lua/plugins/gruvbox-material.lua
@@ -4,6 +4,7 @@ return{
   name = "gruvbox-material",
   priority = 1000,
   config = function()
+    vim.g.gruvbox_material_transparent_background = 2
     vim.cmd.colorscheme "gruvbox-material"
   end
 }
